fe82abd211f64506a29f5e582273da477d28d204
IjazkhanAfridi <iafridi640@gmail.com>
2025-03-06 12:32:33 +0500

    add latest changes

--- server/index.js
+++ server/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const connectDB = require('./config/db');
+require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 connectDB();
+app.get('/', (req, res) => {
+  res.send('API is working!');
+});
 
 app.use('/leads', require('./routes/lead'));
 
